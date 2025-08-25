27e0e642d9268e160d98907aa6063afb1cdda5fa
Mariano Huitron <developer.mhuitron@gmail.com>
2025-08-25 08:03:36 -0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+
+  const logger = new Logger('Payments-ms');
+
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true
+  });
+
+  app.setGlobalPrefix('api');
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+
+  logger.log('Payments Microservice Running on port ', envs.port);
+}
+bootstrap();
