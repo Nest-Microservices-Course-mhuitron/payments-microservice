7ace4bc6ab9c5d390fdfaac3ec4a6f15b4898f97
Mariano Huitron <developer.mhuitron@gmail.com>
2025-08-27 23:24:55 -0700

    feat: implement NATS microservice integration and update environment configurations

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
@@ -19,6 +20,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natServers
+    },
+  }, {inheritAppConfig: true})
+
+  await app.startAllMicroservices();
 
   await app.listen(process.env.PORT ?? 3000);
 
